b6b0c08b8c5fa2ee662f02e373db4d03e1fe990a
Srihari R <rsrihari95@gmail.com>
2015-03-26 23:05:25 +0530

    new

---
+++ sidebar.php
@@ -0,0 +1,51 @@
+<?php
+
+echo " <div id='sidebar-wrapper'>
+            <ul class='sidebar-nav'>
+                <li class='sidebar-brand'>
+                    <a href='index.php'>
+						Candidates 2015
+                    </a>
+                </li>
+				<li>
+						<h5>Presented  by: </h5>
+						<h4 style='color:white'>The Scholars' Avenue</h4>
+				<li>
+                <li>
+                    <a href='index.php'>Vice President</a>
+					<ul>
+					<li style='color:gray'><a href='atal.php'>Atal A Agarwal</a></li>
+					<li style='color:gray'><a href='ketan.php'>Ketan Ingle</a></li>
+					</ul>
+                </li>
+                <li>
+                    <a href='index.php'>GSec Technology</a>
+					<ul>
+					<li style='color:gray'><a href='bibhu.php'>Bibhu Agarwal</a></li>
+					<li style='color:gray'><a href='fatwani.php'>Kartikeya Fatwani</a></li>
+					<li style='color:gray'><a href='palak.php'>Palak Goyal</a></li>
+					<li style='color:gray'><a href='tanveer.php'>Tanveer Shaikh</a></li>
+					</ul>
+
+                </li>
+                <li>
+                    <a href='index.php'>GSec Sports</a>
+					<ul>
+					<li style='color:gray'><a href='shenoy.html'>Gaurav Shenoy</a></li>
+					<li style='color:gray'><a href='prateek.php'>Prateek Jain</a></li>
+					<li style='color:gray'><a href='sanket.php'>Sanket Mondal</a></li>
+					</ul>
+                </li>
+                <li>
+                    <a href='index.php'>GSec Social & Cultural</a>
+					<ul>
+					<li style='color:gray'><a href='ayudh.php'>Ayudh Dutta</a></li>
+					<li style='color:gray'><a href='panda.php'>Gourav Panda</a></li>
+					<li style='color:gray'><a href='vivek.php'>Vivek Bhutra</a></li>
+					</ul>
+                </li>
+            </ul>
+
+        </div>"
+
+?>
